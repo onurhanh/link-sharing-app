a9d68a2563699c1c8fe5e2e2cfaef388dfcdda13
Onur Han <onurhan@Onur-MacBook-Air.local>
2025-03-20 18:43:47 +0300

    edit images

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -15,7 +15,7 @@ export default function Sidebar() {
         <div>
             <div className="p-[16px] flex items-center justify-between bg-[#FFFFFF] rounded-b-lg md:ml-[24px] md:mr-[24px] md:rounded-lg">
                 <button className="items-center flex gap-x-1.5 p-[10px]">
-                    <img src="src/images/link-icon.svg" alt="" />
+                    <img src="public/images/link-icon.svg" alt="" />
                     <p className="font-bold font-stretch-100% text-2xl md:flex hidden">devlinks</p>
                 </button>
                 <div className="flex">
@@ -48,7 +48,7 @@ export default function Sidebar() {
                         </button>
                 </div>
                 <button>
-                    <img src="src/images/publish.svg" alt="" className="md:hidden"/>
+                    <img src="public/images/publish.svg" alt="" className="md:hidden"/>
                     <button className="hidden md:flex pt-[11px] pl-[27px] pb-[11px] pr-[27px] border rounded-lg border-[#6B46C1] text-[#6B46C1] hover:bg-[#EFEBFF] hover:text-[#633CFF] delay-30">Preview</button>
                 </button>
             </div>
