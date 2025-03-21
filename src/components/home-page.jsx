db41dd707a337db714f773576cbbe1f3a9762ecf
Onur Han <onurhan@Onur-MacBook-Air.local>
2025-03-21 17:36:58 +0300

    link fixed

--- src/components/home-page.jsx
+++ src/components/home-page.jsx
@@ -48,22 +48,26 @@ export default function HomePage() {
         <Sidebar />
         <div className="grid grid-cols-6 xl:grid-cols-12 ml-4 mr-4 mb-[32px]">
 
-            <div className="relative col-span-5 hidden xl:flex xl:bg-[#FFFFFF] xl:p-[24px] xl:mr-[10px] xl:ml-[8px] xl:mt-[16px] xl:mb-[24px]  xl:rounded-lg">
-                <img className="hidden relative xl:flex xl:w-[307px] xl:h-[631px] xl:mx-auto xl:mt-[40px]" src="/images/phone.svg" alt="" />
-                <ul>
+            <div className="col-span-5 hidden xl:flex xl:bg-[#FFFFFF] xl:p-[24px] xl:mr-[10px] xl:ml-[8px] xl:mt-[16px] xl:mb-[24px]  xl:rounded-lg">
+                <div className="hidden relative xl:flex xl:w-[307px] xl:h-[631px] xl:mx-auto xl:mt-[40px] z-30" >
+                <img src="/images/phone.svg" alt="" />
+                <div className="absolute w-[238px] h-[46px] top-[279px] start-[33px] z-40">
                     {links.map(link => {
                         const linkPlatform = platforms.find(x => x.key == link.platform);
                         return(
-                            <li className="absolute flex items-center rounded-lg gap-1 p-2 w-[237px] h-[44px] top-85 start-42 " style={{
+                            <div className="flex w-full h-full mb-[16px] items-center rounded-lg gap-1 p-2 " style={{
                                 backgroundColor: linkPlatform?.bgColor,
                                 color: linkPlatform?.textColor
                             }} key={link.id}>
                                 {linkPlatform?.Icon && <linkPlatform.Icon />}
                                 {linkPlatform?.label && <span>{linkPlatform.label}</span>}
-                            </li>
+                                </div>
                         )
                     })}
-                </ul>
+                    </div>
+                </div>
+                   
+                
             </div>
             <div className="col-span-7 md:ml-[9px] md:mr-[9px]">
                 <div className="bg-[#FFFFFF]  rounded-lg">
